7d93cca616a6a40f43a5d557170a9d43e8abffac
abhineetchowdhury21 <abhineet.chowdhury@mitwpu.edu.in>
2026-02-12 20:04:18 +0530

    Update script.js

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -1,3 +1,29 @@
-function changeText() {
-    document.getElementById("text").innerText = "You clicked the button!";
+body {
+    font-family: Arial;
+    background-color: #f2f2f2;
+    text-align: center;
+}
+
+form {
+    background: white;
+    padding: 20px;
+    width: 300px;
+    margin: auto;
+    border-radius: 10px;
+}
+
+input {
+    width: 90%;
+    padding: 5px;
+    margin: 5px;
+}
+
+button {
+    padding: 8px 15px;
+    margin-top: 10px;
+    cursor: pointer;
+}
+
+#message {
+    font-weight: bold;
 }
